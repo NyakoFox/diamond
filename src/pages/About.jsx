1c4cadf32c244fa3141bd29635dcb2ccfd5718e3
NyakoFox <nyakowofox@gmail.com>
2023-06-23 13:19:54 -0300

    Clean up name in about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,11 @@ function About() {
     return (
         <div className="container mx-auto p-4 flex flex-col align-middle justify-items-center w-full">
             <h1 className="text-3xl font-bold text-gray-400 dark:text-gray text-center">I&apos;m <span className="text-gray-800 dark:text-white text-4xl">Nyako</span>.</h1>
-            <p className="text-gray-600 dark:text-gray-300 text-center">Or Ally, or Alexia, or Alexandra, or-</p>
+            <p className="text-gray-600 dark:text-gray-300 text-center">Or Ally, if you want a real name.</p>
 
             <section className="mt-8">
                 <p className="text-gray-600 dark:text-gray-300 text-center">I&apos;m a software developer, musician, and artist (sometimes).</p>
+                <p className="text-gray-600 dark:text-gray-300 text-center">I&apos;m 18 years old and transgender. (she/her)</p>
                 <p className="text-gray-600 dark:text-gray-300 text-center">I like to make things.</p>
             </section>
 
